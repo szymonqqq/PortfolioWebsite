badb854a26aedce03d970f6daafc9146fecb82f8
Szymon Ekman <szymonklam17@gmail.com>
2024-05-31 00:15:53 +0200

    add typewriter, add style and about me

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MobileNav from "./MobileNav";
 import TypeWriter from "./TypeWriter";
 import MainPageImgBox from "./MainPageImgBox";
+import AboutMe from "./AboutMe";
 
 
 const MainPage = () => {
@@ -9,8 +10,14 @@ const MainPage = () => {
     return(
         <>
         <MobileNav/>
-        <TypeWriter/>
-        <MainPageImgBox/>
+        {/* <TypeWriter text={["Hi,", "I'm Szymon", "Full Stack Developer.", "I'm passionate about", "crafting beautiful", "and intuitive, ", "user interfaces.", "About me ->","Projects ->"]}
+        
+
+
+        
+        /> */}
+        {/* <MainPageImgBox/> */}
+        <AboutMe/>
         
         
         
--- src/components/TypeWriter.tsx
+++ src/components/TypeWriter.tsx
@@ -2,31 +2,58 @@ import React, { useEffect, useState } from "react";
 import './styles/type_writer.css'
 
 
+interface TypeWriterProps {
+    text: string[];
+}
 
-
-const TypeWriter = () =>{
+const TypeWriter: React.FC<TypeWriterProps>= ({text}) =>{
     const [unvisibleBorder, setuUnvisibleBorder] = useState([])
-    const [text, setText] = useState("h isss")
-    const [show, setShowText] = useState("")
-    let i =0
-
-
-
+    const [show, setShowText] = useState<string[]>(text.map(()=>""))
+    let i = 0
+    let j=0
+  
 useEffect(()=>{
     const pp = setInterval(()=>{
-        // setShowText((prev) => prev + text[i])
         
-       i++
-    }, 1000)
-    return () => clearInterval(pp); 
    
-},[])
-console.log(show)
+     
+        if(i<=text[j].length-1){
+            let txt = show[j] + text[j][i]
+            show[j] = txt
+       
+            setShowText(prev =>{
+                let updatedShow = [...prev]; 
+                updatedShow[j]=txt
+                return updatedShow;
+            }
+          
+            )
+            i++
+        }
+           else if(j<text.length-1){
+            j++
+            i=0
+            
+           }
+        
+       
+    }, 100)
+   
+    return () => clearInterval(pp); 
+ 
+   
+    
+},[text])
+const genE = show.map((txt, index) => (
+    <p key={index} style={txt!==text[index]&&txt!==""?{borderRight: "1px solid black"}:{}}> {txt}</p>
+));
 return(
 <div className="text_box">
-    <p>{show}</p>
-    {/* <p>Hi,</p> */}
-    {/* <p>I'm Szymon</p> 
+   
+
+    {genE}  
+     {/* <p>Hi,</p> 
+    <p>I'm Szymon</p> 
     <p>Full Stack Developer.</p>
     <p>I'm passionate about</p> 
     <p>crafting beautiful</p>
@@ -34,7 +61,7 @@ return(
     <p>user interfaces.</p>
    
     <p>About me {"->"}</p>
-    <p>Projects{"->"}</p> */}
+    <p>Projects{"->"}</p>  */}
 </div>
 )
 
